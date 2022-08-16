77b2bf2a221d803cffc13f830e7668cec61f29a3
abdulloh19 <parij4320@gmail.com>
2022-08-16 23:30:53 +0500

    added slider

--- js/script.js
+++ js/script.js
@@ -290,4 +290,51 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal()
     }, 4000)
   }
+
+  //Slider
+
+  const slides = document.querySelectorAll('.offer__slide'),
+    next = document.querySelector('.offer__slider-next'),
+    prev = document.querySelector('.offer__slider-prev'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current')
+
+    if(slides.length < 10) {
+      total.textContent = `0${slides.length}` 
+    } else {
+      total.textContent = slides.length
+    }
+
+  let slideIndex = 1
+
+  showSlides(slideIndex)
+
+  function showSlides(idx) {
+    if(idx > slides.length) {
+      slideIndex = 1
+    }
+    if(idx < 1) {
+      slideIndex = slides.length
+    }
+    slides.forEach(item => item.style.display = 'none')
+    slides[slideIndex - 1].style.display = 'block'
+
+    if(slides.length < 10) {
+      current.textContent = `0${slideIndex}`
+    } else {
+      current.textContent = slideIndex
+    }
+  } 
+
+  function plusSlides(idx) {
+    showSlides(slideIndex += idx)
+  }
+
+  next.addEventListener('click', () => {
+    plusSlides(1)
+  })
+
+  prev.addEventListener('click', () => {
+    plusSlides(-1)
+  })
 })
